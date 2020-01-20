1a47ec1062d905cea367347a991d1f3efbe55c54
defacto2k15 <bczerwinski462@gmail.com>
2020-01-21 00:01:27 +0100

    Added start animation

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -2,16 +2,16 @@ using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-[RequireComponent(typeof (GUITexture))]
-public class ForcedReset : MonoBehaviour
-{
-    private void Update()
-    {
-        // if we have forced a reset ...
-        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
-        {
-            //... reload the scene
-            Application.LoadLevelAsync(Application.loadedLevelName);
-        }
-    }
-}
+//[RequireComponent(typeof (UI.Im))]
+//public class ForcedReset : MonoBehaviour
+//{
+//    private void Update()
+//    {
+//        // if we have forced a reset ...
+//        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
+//        {
+//            //... reload the scene
+//            Application.LoadLevelAsync(Application.loadedLevelName);
+//        }
+//    }
+//}
